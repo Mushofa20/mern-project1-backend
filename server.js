89cd0877e81dab1d28396bb2695257751a9c91ca
Mushofa20 <ahmadmushofasalsabil@gmail.com>
2025-01-23 15:54:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Konfigurasi CORS
 const corsOptions = {
-    origin: "http://localhost:3000", // Membolehkan akses dari frontend di localhost:3000
+    origin: "https://mern-project1-frontend-alpha.vercel.app/", // Membolehkan akses dari frontend di vercel
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
 };
